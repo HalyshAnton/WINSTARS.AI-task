dd20e07de262048b15c379e7a46545779eb75a4b
HalyshAnton <kyhar007@gmail.com>
2025-02-14 17:49:33 +0200

    add MnistCNN

--- task1/models.py
+++ task1/models.py
@@ -159,7 +159,6 @@ class MnistFeedForward(MnistClassifierInterface):
             X (numpy.ndarray): input data of shape (N, 1, 28, 28)
 
         Returns:
-
             numpy.ndarray: digit probabilities of shape (N, 10)
         """
         X_prep = self.__preprocess(X)
@@ -180,6 +179,110 @@ class MnistFeedForward(MnistClassifierInterface):
             torch.Tensor: preprocessed input of shape (N, 784)
         """
         return torch.tensor(X.reshape(-1, 28*28),
+                            dtype=torch.float32,
+                            device=self.device
+                            )
+
+
+class MnistCNN(MnistClassifierInterface):
+    """
+    Convolutional Neural Network model for classifying MNIST images
+    """
+
+    def __init__(self, epochs=10, batch=32, lr=1e-4):
+        """
+        Initializes the MnistCNN model
+
+        Args:
+            epochs (int, optional): number of training epochs (default is 10)
+            batch (int, optional): batch size for training (default is 32)
+            lr (float, optional): learning rate for the optimizer (default is 1e-4)
+        """
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding='same'),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 32, kernel_size=3, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+            nn.Linear(32*7*7, 128),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.Softmax(dim=1)
+        )
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = self.model.to(self.device)
+
+        self.epochs = epochs
+        self.batch_size = batch
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                          lr=lr
+                                          )
+
+    def train(self, X, y):
+        """
+        Trains the model on the provided training data
+
+        Args:
+            X (numpy.ndarray): input training data of shape (N, 1, 28, 28)
+            y (numpy.ndarray): labels for the training data of shape (N,)
+        """
+        X_prep = self.__preprocess(X)
+
+        dataset = list(zip(X_prep, y))
+        dataloader = torch.utils.data.DataLoader(dataset,
+                                                 batch_size=self.batch_size,
+                                                 shuffle=True
+                                                 )
+
+        loss_fn = nn.CrossEntropyLoss()
+
+        self.model.train()
+        for _ in range(self.epochs):
+            for X_batch, y_batch in dataloader:
+                y_batch = y_batch.to(self.device)
+
+                y_pred = self.model(X_batch)
+                loss = loss_fn(y_pred, y_batch)
+
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+    def predict(self, X):
+        """
+        Makes predictions on the provided input data
+
+        Args:
+            X (numpy.ndarray): input data of shape (N, 1, 28, 28)
+
+        Returns:
+            numpy.ndarray: digit probabilities of shape (N, 10)
+        """
+        X_prep = self.__preprocess(X)
+        X_prep = X_prep.to(self.device)
+
+        self.model.eval()
+        with torch.no_grad():
+            return self.model(X_prep).cpu().numpy()
+
+    def __preprocess(self, X):
+        """
+        Convert data to tensor
+
+        Args:
+            X (numpy.ndarray): input data of shape (N, 1, 28, 28)
+
+        Returns:
+            torch.Tensor: processed data
+        """
+        return torch.tensor(X,
                             dtype=torch.float32,
                             device=self.device
                             )
